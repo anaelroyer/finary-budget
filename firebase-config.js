1a2f69b7d994c1c55f7993186a744e6e1a916c0e
anaelroyer <anaelroyer29@gmail.com>
2026-08-27 22:18:09 +0200

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.BETA_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCynPsbSGXlEO_a1P7MSkEgHulcHRc7zwo",
-  authDomain: "budget-patrimoine-beta.firebaseapp.com",
-  projectId: "budget-patrimoine-beta",
-  storageBucket: "budget-patrimoine-beta.firebasestorage.app",
-  messagingSenderId: "274422485653",
-  appId: "1:274422485653:web:13345f1040857f3222c19c"
-};
