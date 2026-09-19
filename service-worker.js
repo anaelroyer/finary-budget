412ebf008c24cf24870064b5456d463d679875a1
anaelroyer <anaelroyer29@gmail.com>
2026-09-19 14:05:23 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='finary-budget-v28-9-2';
+const CACHE_NAME='finary-budget-v28-9-3';
 const CORE=['./','./app.html','./manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE).catch(()=>{})));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
